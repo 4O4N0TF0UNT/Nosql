bc2be33a7c8879b04f20276203e31ed27778d2ef
4O4N0TF0UNT <2943520691@qq.com>
2022-07-13 15:45:05 +0800

    nosql

---
+++ 项目/rank/src/main/java/ranking/entity/DynamicInfo.java
@@ -0,0 +1,24 @@
+package ranking.entity;
+
+
+public class DynamicInfo {
+
+    private String phone;
+    private String time;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ 项目/rank/src/main/java/ranking/entity/PhoneInfo.java
@@ -0,0 +1,35 @@
+package ranking.entity;
+
+
+public class PhoneInfo {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer sales;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+}
